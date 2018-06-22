5397c8c57daa1188c3bec9b3e5aa523f656864e6
Matthew <32231254+ibroadsword@users.noreply.github.com>
2018-06-22 12:24:54 -0500

    Published Version

--- js/master.js
+++ js/master.js
@@ -1,10 +1,29 @@
+//Script to open Dropdown on sidebar
+var dropdown = function() {
+    var menu = document.getElementById("dropdown-menu");
+    if (menu.className.indexOf("w3-show") == -1) {
+        menu.className += " w3-show";
+    } else { 
+        menu.className = menu.className.replace(" w3-show", "");
+    }
+}
+
 // Script to open and close sidebar
-function w3_open() {
+var w3Open = function() {
+    document.querySelector("nav").classList.add("w3-animate-left");
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
 }
  
-function w3_close() {
+var w3Close = function() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
+    document.querySelector("nav").classList.remove("w3-animate-left");
 }
+
+// function spin() {
+//     setTimeout(function() {
+//         document.querySelector("body").classList.add("w3-spin");
+//     }, 23500);
+// }
+// spin();
